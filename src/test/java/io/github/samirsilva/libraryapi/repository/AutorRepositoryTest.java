64fb327372a53a211287c4882c388578efd9ad7b
Samir Silva <samirsilva030@gmail.com>
2026-03-12 04:36:02 -0300

    OnetoMany relationship with cascade

--- src/test/java/io/github/samirsilva/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/samirsilva/libraryapi/repository/AutorRepositoryTest.java
@@ -1,11 +1,15 @@
 package io.github.samirsilva.libraryapi.repository;
 
 import io.github.samirsilva.libraryapi.model.Autor;
+import io.github.samirsilva.libraryapi.model.GeneroLivro;
+import io.github.samirsilva.libraryapi.model.Livro;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +19,9 @@ public class AutorRepositoryTest {
 
     @Autowired
     AutorRepository repository;
+
+    @Autowired
+    LivroRepository livroRepository;
 
     @Test
     public void salvarTest(){
@@ -68,5 +75,38 @@ public class AutorRepositoryTest {
         var id = UUID.fromString("e2ef7b66-b416-4e0f-a681-7439ce8c8694");
         var maria = repository.findById(id).get();
         repository.delete(maria);
+    }
+    @Test
+    void salvarAutorComLivrosTest(){
+        Autor autor = new Autor();
+        autor.setNome("Antonio");
+        autor.setNacionalidade("Americana");
+        autor.setDataNascimento(LocalDate.of(1970,8,5));
+
+        Livro livro = new Livro();
+        livro.setIsbn("20847-84874");
+        livro.setPreco(BigDecimal.valueOf(204));
+        livro.setGenero(GeneroLivro.MISTERIO);
+        livro.setTitulo("O roubo da casa assombrada");
+        livro.setDataPublicacao(LocalDate.of(1933, 1, 2));
+        livro.setAutor(autor);
+
+        Livro livro2 = new Livro();
+        livro2.setIsbn("99999-84874");
+        livro2.setPreco(BigDecimal.valueOf(650));
+        livro2.setGenero(GeneroLivro.MISTERIO);
+        livro2.setTitulo("O roubo da casa assombrada");
+        livro2.setDataPublicacao(LocalDate.of(2000, 1, 2));
+        livro2.setAutor(autor);
+
+
+        autor.setLivros(new ArrayList<>());
+        autor.getLivros().add(livro);
+        autor.getLivros().add(livro2);
+
+        repository.save(autor);
+
+        //livroRepository.saveAll(autor.getLivros());
+
     }
 }
