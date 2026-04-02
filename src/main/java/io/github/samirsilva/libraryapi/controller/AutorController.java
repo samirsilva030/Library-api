421899f3370402934e79c76fafa2186fd3e5ed11
Samir Silva <samirsilva030@gmail.com>
2026-04-02 18:42:01 -0300

    search authors with filters

--- src/main/java/io/github/samirsilva/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/samirsilva/libraryapi/controller/AutorController.java
@@ -9,8 +9,10 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.security.Provider;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("autores")
@@ -61,4 +63,15 @@ public class AutorController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+        @RequestParam(value = "nome", required = false) String nome,
+        @RequestParam(value = "nacionalidade", required = false) String nacionialidade){
+        List<Autor> resultado = service.pesquisa(nome,nacionialidade);
+        List<AutorDTO> lista = resultado.stream().map(autor -> new AutorDTO(autor.getId(), autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade())).collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+    }
+
 }
--- src/main/java/io/github/samirsilva/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/samirsilva/libraryapi/repository/AutorRepository.java
@@ -4,9 +4,13 @@ import io.github.samirsilva.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
