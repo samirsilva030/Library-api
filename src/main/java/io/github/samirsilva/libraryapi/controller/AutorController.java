ed39e0ddc6eabf31d094d11d8b04822bb033d066
Samir Silva <samirsilva030@gmail.com>
2026-04-09 18:29:54 -0300

    add more validations

--- src/main/java/io/github/samirsilva/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/samirsilva/libraryapi/controller/AutorController.java
@@ -87,7 +87,7 @@ public class AutorController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto){
+    public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody @Valid AutorDTO dto){
        try {
            var idAutor = UUID.fromString(id);
            Optional<Autor> autorOptional = service.obterPorId(idAutor);
